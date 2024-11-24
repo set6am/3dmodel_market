e6aeb7935c349963802b55a013ce4df394e49819
set6am <set6am@yandex.ru>
2024-11-24 19:04:15 +0500

    create custom user model with some deleted fields

--- market_app/users/models.py
+++ market_app/users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):   
+    REQUIRED_FIELDS = ['email']
+    # unnecessary fields now 
+    groups = None
+    user_permissions = None
+    
+    # moved to Profile model
+    first_name = None
+    last_name = None
