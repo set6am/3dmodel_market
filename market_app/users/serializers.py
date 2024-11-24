632c344c18c943e9617345b01d88091304532d3a
set6am <set6am@yandex.ru>
2024-11-24 19:05:07 +0500

    add comments

--- market_app/users/serializers.py
+++ market_app/users/serializers.py
@@ -3,7 +3,10 @@ from rest_framework_simplejwt import serializers
 
 
 class CustomTokenObtainPairSerializer(serializers.TokenObtainPairSerializer):
+    """
+    Custom serializer for return only access jwt token without refresh token.
+    """
     def validate(self, attrs: Dict[str, Any]) -> Dict[str, str]:
             data = super().validate(attrs)
-            data.pop('refresh', None)  # delete refresh token, only access token return
+            data.pop('refresh', None) 
             return data
